6d17a0f8632de001afa52393d332a269b5807370
naga55 <nagamine.sayaka@da-vinci-studio.com>
2020-09-23 15:29:41 +0900

    test

--- app/models/create_article_and_send_mail.rb
+++ app/models/create_article_and_send_mail.rb
@@ -1,13 +1,22 @@
 class CreateArticleAndSendMail
   include ActiveModel::Model
+  define_model_callbacks :save, only: :after
+  after_save :send_mail
+  attr_reader :article
 
   def initialize(article)
     @article = article
   end
 
   def save
-    if @article.save
-      ArticleMailer.new_article(@article).deliver_now
+    run_callbacks :save do
+      @article.save
     end
   end
+
+  private
+
+    def send_mail
+      ArticleMailer.new_article(@article).deliver_now
+    end
 end
